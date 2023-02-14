4e0cdb695f40cbbada9c0bad5e7bf38d18a9019d
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-14 20:16:23 +0530

    added: Greeting message for home page

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicalProgramming
+{
+    public class Home
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the solutions page for Day-6 LogicalProgramming Assignments!");
+        }
+    }
+}
